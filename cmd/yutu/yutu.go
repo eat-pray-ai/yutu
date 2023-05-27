1165aa3b5bbef7212c89ad92ecc065c88231791b
chaoqun <27287694+OpenWaygate@users.noreply.github.com>
2023-05-28 00:54:42 +0800

    :recycle: Refactor auth to get code from redirectURL

--- cmd/yutu/yutu.go
+++ cmd/yutu/yutu.go
@@ -2,32 +2,17 @@ package main
 
 import (
 	"context"
-	"log"
-	"os"
 
 	"github.com/eat-pray-ai/yutu/pkg/auth"
 	"github.com/eat-pray-ai/yutu/pkg/you2be"
 
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 )
 
 func main() {
-	ctx := context.Background()
-
-	b, err := os.ReadFile("client_secret_desktop.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret: %v", err)
-	}
-
-	config, err := google.ConfigFromJSON(b, youtube.YoutubeReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file %s to config: %v", string(b), err)
-	}
-
-	client := auth.GetClient(ctx, config)
-	service, err := youtube.NewService(ctx, option.WithHTTPClient(client))
+	client := auth.GetClient(youtube.YoutubeReadonlyScope)
+	service, err := youtube.NewService(context.Background(), option.WithHTTPClient(client))
 	auth.HandleError(err, "Error creating YouTube client")
 
 	you2be.ChannelsListById(service, []string{"snippet", "contentDetails", "statistics"}, "***REMOVED***")
--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -5,20 +5,31 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"os/user"
 	"path/filepath"
+	"runtime"
 
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
-const missingClientSecretsMessage = `
-Please configure OAuth 2.0
-`
+func GetClient(scope string) *http.Client {
+	ctx := context.Background()
+	b, err := os.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret: %v", err)
+	}
 
-func GetClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	config, err := google.ConfigFromJSON(b, scope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret to config: %v", err)
+	}
+
 	cacheFile, err := tokenCacheFile()
 	if err != nil {
 		log.Fatalf("Unable to get path to cached credential. %v", err)
@@ -26,22 +37,70 @@ func GetClient(ctx context.Context, config *oauth2.Config) *http.Client {
 
 	token, err := tokenFromFile(cacheFile)
 	if err != nil {
-		token = getTokenFromWeb(config)
+		authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+		fmt.Println("Getting token from web...")
+		token = getTokenFromWeb(config, authURL)
 		saveToken(cacheFile, token)
 	}
 
 	return config.Client(ctx, token)
 }
 
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
-
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
+func startWebServer(redirectURL string) (codeCh chan string, err error) {
+	u, err := url.Parse(redirectURL)
+	if err != nil {
+		return nil, err
 	}
 
+	listener, err := net.Listen("tcp", u.Host)
+	if err != nil {
+		return nil, err
+	}
+
+	codeCh = make(chan string)
+	go http.Serve(listener, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		code := r.FormValue("code")
+		codeCh <- code
+		listener.Close()
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "Received code: %v\r\nYou can now safely close this window.", code)
+	}))
+
+	return codeCh, err
+}
+
+func openURL(url string) error {
+	var err error
+	switch os := runtime.GOOS; os {
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("cannot open URL %s on this platform", url)
+	}
+
+	return err
+}
+
+func getTokenFromWeb(config *oauth2.Config, authURL string) *oauth2.Token {
+	codeCh, err := startWebServer(config.RedirectURL)
+	if err != nil {
+		log.Fatalf("Unable to start a web server %v", err)
+	}
+
+	if err := openURL(authURL); err != nil {
+		log.Fatalf("Unable to open authorization URL in browser: %v", err)
+	} else {
+		fmt.Printf(`Your browser has been opened to an authorization URL.
+This program will resume once authorization has been provided.
+If nothing happens, open the following URL in your browser manually:
+%v`, authURL)
+	}
+
+	code := <-codeCh
 	token, err := config.Exchange(context.TODO(), code)
 	if err != nil {
 		log.Fatalf("Unable to retrieve token from web %v", err)
@@ -58,7 +117,7 @@ func tokenCacheFile() (string, error) {
 
 	tokenCacheDir := filepath.Join(user.HomeDir, ".credentials")
 	os.MkdirAll(tokenCacheDir, 0700)
-	return filepath.Join(tokenCacheDir, url.QueryEscape("youtube.json")), err
+	return filepath.Join(tokenCacheDir, url.QueryEscape("yutu.json")), err
 }
 
 func tokenFromFile(file string) (*oauth2.Token, error) {
