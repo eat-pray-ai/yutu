324f38a2c2f614d9047ab1c430ccc294f1b61764
chaoqun <27287694+OpenWaygate@users.noreply.github.com>
2024-02-05 22:25:44 +0800

    :fire: Remove unused config

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,11 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "yutu",
@@ -16,12 +13,6 @@ var rootCmd = &cobra.Command{
 	Long:  `yutu is a fully functional CLI for YouTube, which can be used to manupulate YouTube videos, playlists, channels, etc.`,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-		yutuDir := home + "/.yutu"
-		if _, err := os.Stat(yutuDir); os.IsNotExist(err) {
-			os.Mkdir(yutuDir, 0755)
-		}
 		cmd.Help()
 	},
 }
@@ -35,25 +26,8 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.yutu/config.yaml)")
 }
 
 func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		viper.AddConfigPath(home + "/.yutu")
-		viper.SetConfigType("yaml")
-		viper.SetConfigName("config")
-	}
-
 	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
