e391e87c6bf1fe03445aea8e7d6f4d3299c39520
OpenWaygate <27287694+OpenWaygate@users.noreply.github.com>
2024-04-22 23:12:40 +0800

    :sparkles: Add i18nLanguage and i18nRegion service

---
+++ pkg/yutuber/i18nLanguage.go
@@ -0,0 +1,60 @@
+package yutuber
+
+import (
+	"errors"
+	"fmt"
+	"github.com/eat-pray-ai/yutu/pkg/utils"
+	"google.golang.org/api/youtube/v3"
+	"log"
+)
+
+var (
+	errGetI18nLanguage = errors.New("failed to get i18n language")
+)
+
+type i18nLanguage struct{}
+
+type I18nLanguage interface {
+	get(parts []string) []*youtube.I18nLanguage
+	List(parts []string, output string)
+}
+
+type I18nLanguageOption func(*i18nLanguage)
+
+func NewI18nLanguage(opts ...I18nLanguageOption) I18nLanguage {
+	i := &i18nLanguage{}
+
+	for _, opt := range opts {
+		opt(i)
+	}
+
+	return i
+}
+
+func (i *i18nLanguage) get(parts []string) []*youtube.I18nLanguage {
+	call := service.I18nLanguages.List(parts)
+	response, err := call.Do()
+	if err != nil {
+		log.Fatalln(errors.Join(errGetI18nLanguage, err))
+	}
+
+	return response.Items
+}
+
+func (i *i18nLanguage) List(parts []string, output string) {
+	i18nLanguages := i.get(parts)
+	switch output {
+	case "json":
+		utils.PrintJSON(i18nLanguages)
+	case "yaml":
+		utils.PrintYAML(i18nLanguages)
+	default:
+		fmt.Println("id\thl\tname")
+		for _, i18nLanguage := range i18nLanguages {
+			fmt.Printf(
+				"%v\t%v\t%v\n",
+				i18nLanguage.Id, i18nLanguage.Snippet.Hl, i18nLanguage.Snippet.Name,
+			)
+		}
+	}
+}
---
+++ pkg/yutuber/i18nRegion.go
@@ -0,0 +1,60 @@
+package yutuber
+
+import (
+	"errors"
+	"fmt"
+	"github.com/eat-pray-ai/yutu/pkg/utils"
+	"google.golang.org/api/youtube/v3"
+	"log"
+)
+
+var (
+	errGetI18nRegion = errors.New("failed to get i18n region")
+)
+
+type i18nRegion struct{}
+
+type I18nRegion interface {
+	get(parts []string) []*youtube.I18nRegion
+	List(parts []string, output string)
+}
+
+type I18nRegionOption func(*i18nRegion)
+
+func NewI18nRegion(opts ...I18nRegionOption) I18nRegion {
+	i := &i18nRegion{}
+
+	for _, opt := range opts {
+		opt(i)
+	}
+
+	return i
+}
+
+func (i *i18nRegion) get(parts []string) []*youtube.I18nRegion {
+	call := service.I18nRegions.List(parts)
+	response, err := call.Do()
+	if err != nil {
+		log.Fatalln(errors.Join(errGetI18nRegion, err))
+	}
+
+	return response.Items
+}
+
+func (i *i18nRegion) List(parts []string, output string) {
+	i18nRegions := i.get(parts)
+	switch output {
+	case "json":
+		utils.PrintJSON(i18nRegions)
+	case "yaml":
+		utils.PrintYAML(i18nRegions)
+	default:
+		fmt.Println("Id\tgl\tname")
+		for _, i18nRegion := range i18nRegions {
+			fmt.Printf(
+				"%v\t%v\t%v\n",
+				i18nRegion.Id, i18nRegion.Snippet.Gl, i18nRegion.Snippet.Name,
+			)
+		}
+	}
+}
