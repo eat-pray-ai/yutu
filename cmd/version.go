3daf01a6c8d757eaa766fc0ac511e4646b0c14fa
chaoqun <27287694+OpenWaygate@users.noreply.github.com>
2024-05-30 22:56:53 +0800

    :speech_balloon: Update version output

--- cmd/version.go
+++ cmd/version.go
@@ -1,6 +1,9 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/savioxavier/termlink"
+	"github.com/spf13/cobra"
+)
 
 var (
 	Version    = ""
@@ -8,6 +11,7 @@ var (
 	CommitDate = ""
 	Os         = ""
 	Arch       = ""
+	repo       = "https://github.com/eat-pray-ai/yutu"
 )
 
 var versionCmd = &cobra.Command{
@@ -15,7 +19,17 @@ var versionCmd = &cobra.Command{
 	Short: "Show the version of yutu",
 	Long:  "Show the version of yutu",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Printf("yutu version %s %s/%s\nbuild %s-%s\n", Version, Os, Arch, Commit, CommitDate)
+		cmd.Printf("yutu🐰 version %s", Version)
+		if Os != "" && Arch != "" {
+			cmd.Printf(" %s/%s", Os, Arch)
+		}
+		if Commit != "" && CommitDate != "" {
+			cmd.Printf("\nbuild %s-%s", Commit, CommitDate)
+		}
+
+		cmd.Println()
+		cmd.Println("Star🌟:", termlink.Link("Github/eat-pray-ai/yutu", repo))
+
 	},
 }
 
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.4 // indirect
+	github.com/savioxavier/termlink v1.3.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0 // indirect
 	go.opentelemetry.io/otel v1.27.0 // indirect
