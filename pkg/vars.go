170e392cb354e7a973ac7e5c372dd5eb50f6a05e
OpenWaygate <27287694+OpenWaygate@users.noreply.github.com>
2025-10-23 23:55:26 +0800

    :sparkles: Improve root dir handling

--- pkg/vars.go
+++ pkg/vars.go
@@ -17,6 +17,9 @@ const (
 	JPUsage     = "JSONPath expression to filter the output"
 	JsonMIME    = "application/json"
 	PerPage     = 20
+
+	getWdFailed    = "failed to get working directory"
+	openRootFailed = "failed to open root directory"
 )
 
 var (
@@ -26,20 +29,6 @@ var (
 )
 
 func init() {
-	var err error
-	rootDir, ok := os.LookupEnv("YUTU_ROOT")
-	if !ok {
-		rootDir = "/"
-		if runtime.GOOS == "windows" {
-			rootDir = os.Getenv("SystemDrive") + `\`
-		}
-	}
-	RootDir = &rootDir
-	Root, err = os.OpenRoot(*RootDir)
-	if err != nil {
-		panic(err)
-	}
-
 	logLevel := slog.LevelInfo
 	if lvl, ok := os.LookupEnv("YUTU_LOG_LEVEL"); ok {
 		switch lvl {
@@ -62,4 +51,24 @@ func init() {
 	)
 
 	slog.SetDefault(Logger)
+
+	var err error
+	rootDir, ok := os.LookupEnv("YUTU_ROOT")
+	if !ok {
+		rootDir, err = os.Getwd()
+		if err != nil {
+			rootDir = "/"
+			if runtime.GOOS == "windows" {
+				rootDir = os.Getenv("SystemDrive") + `\`
+			}
+			slog.Debug(getWdFailed, "error", err, "fallback", rootDir)
+		}
+	}
+	RootDir = &rootDir
+	Root, err = os.OpenRoot(*RootDir)
+	if err != nil {
+		slog.Error(openRootFailed, "dir", *RootDir, "error", err)
+		panic(err)
+	}
+	slog.Debug("Root directory set", "dir", *RootDir)
 }
