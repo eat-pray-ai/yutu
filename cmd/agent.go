faefd73622b4c4d28ed35b721f5d6c1b2397df48
OpenWaygate <27287694+OpenWaygate@users.noreply.github.com>
2025-12-07 00:30:05 +0800

    :boom: Init agent support

---
+++ cmd/agent.go
@@ -0,0 +1,98 @@
+package cmd
+
+import (
+	_ "embed"
+	"log/slog"
+	"os"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+	"github.com/spf13/cobra"
+	"google.golang.org/adk/agent"
+	"google.golang.org/adk/agent/llmagent"
+	"google.golang.org/adk/cmd/launcher"
+	"google.golang.org/adk/cmd/launcher/full"
+	"google.golang.org/adk/model/gemini"
+	"google.golang.org/adk/tool"
+	"google.golang.org/adk/tool/mcptoolset"
+	"google.golang.org/genai"
+)
+
+const (
+	agentShort = "Start agent to automate YouTube workflows"
+	agentLong  = "Start agent to automate YouTube workflows"
+)
+
+var (
+	//go:embed INSTRUCTION.md
+	instruction string
+)
+
+var agentCmd = &cobra.Command{
+	Use:   "agent",
+	Short: agentShort,
+	Long:  agentLong,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		envInstruction, ok := os.LookupEnv("YUTU_AGENT_INSTRUCTION")
+		if ok && envInstruction != "" {
+			instruction = envInstruction
+		}
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := cmd.Context()
+		m, err := gemini.NewModel(
+			ctx, os.Getenv("GEMINI_MODEL"), &genai.ClientConfig{
+				APIKey:  os.Getenv("GEMINI_API_KEY"),
+				Backend: genai.BackendGeminiAPI,
+			},
+		)
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to create Gemini model", "error", err)
+			os.Exit(1)
+		}
+
+		clientTransport, serverTransport := mcp.NewInMemoryTransports()
+		_, err = Server.Connect(ctx, serverTransport, nil)
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to connect to MCP server", "error", err)
+			os.Exit(1)
+		}
+
+		mcpToolSet, err := mcptoolset.New(
+			mcptoolset.Config{
+				Transport: clientTransport,
+			},
+		)
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to create MCP tool set", "error", err)
+		}
+
+		a, err := llmagent.New(
+			llmagent.Config{
+				Model:       m,
+				Name:        "YouTube Copilot",
+				Instruction: instruction,
+				Toolsets:    []tool.Toolset{mcpToolSet},
+			},
+		)
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to create LLM agent", "error", err)
+			os.Exit(1)
+		}
+
+		config := &launcher.Config{
+			AgentLoader: agent.NewSingleLoader(a),
+		}
+		l := full.NewLauncher()
+		if err := l.Execute(ctx, config, os.Args[2:]); err != nil {
+			slog.ErrorContext(
+				ctx, "failed to launch agent",
+				"launch", l.CommandLineSyntax(), "error", err,
+			)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(agentCmd)
+}
--- go.mod
+++ go.mod
@@ -11,31 +11,43 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.123.0 // indirect
 	cloud.google.com/go/auth v0.17.0 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	github.com/a2aproject/a2a-go v0.3.0 // indirect
+	github.com/awalterschulze/gographviz v2.0.3+incompatible // indirect
 	github.com/clipperhouse/uax29/v2 v2.2.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.6 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/safehtml v0.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.7 // indirect
 	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.63.0 // indirect
 	go.opentelemetry.io/otel v1.38.0 // indirect
 	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.38.0 // indirect
 	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251103181224-f26f9409b101 // indirect
 	google.golang.org/grpc v1.76.0 // indirect
+	rsc.io/omap v1.2.0 // indirect
+	rsc.io/ordered v1.1.1 // indirect
 )
 
 require (
@@ -53,7 +65,9 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/adk v0.2.0
 	google.golang.org/api v0.256.0
+	google.golang.org/genai v1.36.0
 	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/yaml.v3 v3.0.1
 )
