ec1d283ff51dd9717ce64e78bc0b69e295e967ec
OpenWaygate <27287694+OpenWaygate@users.noreply.github.com>
2026-08-11 23:45:11 +0800

    :wrench: Manage go-winres as tool dependency

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ import (
 	_ "github.com/eat-pray-ai/yutu/cmd/watermark"
 )
 
-//go:generate go-winres make --arch amd64,arm64 --product-version git-tag --file-version git-tag
+//go:generate go tool go-winres make --arch amd64,arm64 --product-version git-tag --file-version git-tag
 func main() {
 	cmd.Execute()
 }
